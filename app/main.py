c32cd1bd429a242354a229b9f23e3f597a94a3f3
Pontus Backman <ponbac@student.chalmers.se>
2021-05-06 21:55:52 +0200

    moved common dependencies into its own file

--- app/main.py
+++ app/main.py
@@ -2,31 +2,24 @@ import os
 from datetime import datetime, timedelta
 from typing import Optional
 from requests import get
-from jose import JWTError, jwt
+from jose import jwt
 
 from fastapi import Depends, FastAPI, HTTPException, status, Form, Response
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, HTMLResponse, FileResponse
 
 import app.db as db
+from app.dependencies import get_current_user
 from app.utils import is_not_empty, write_to_file
 from app.routers import ninja, stash, poe_auth
 from app.schemas.schemas import Token, TokenData, User, UserInDB
 from .config import settings
 
-# To get a string like this run:
 # openssl rand -hex 32
-# EXAMPLE KEY, NOT USED IN PROD!
-SECRET_KEY = settings.SECRET_KEY
-ALGORITHM = settings.ALGORITHM
-ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES # 48 hours
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
-app.include_router(poe_auth.router) # official poe api-router
+app.include_router(poe_auth.router) # poe oauth2-router
 app.include_router(ninja.router) # poe.ninja-router
 app.include_router(stash.router) # stash-router
 
@@ -46,34 +39,8 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     else:
         expire = datetime.utcnow() + timedelta(hours=2)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = db.get_firebase_user(username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
 
 
 @ app.post("/register", response_model=User)
@@ -112,7 +79,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
@@ -120,13 +87,13 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 
 
 @ app.get("/users/me/", response_model=UserInDB)
-async def read_users_me(current_user: UserInDB = Depends(get_current_active_user)):
+async def read_users_me(current_user: UserInDB = Depends(get_current_user)):
     current_user.hashed_password = 'SECRET'
     return current_user
 
 
 @ app.post("/users/me/friends/add", response_model=User)
-async def add_friend_to_current_user(username_to_add: str = Form(...), current_user: UserInDB = Depends(get_current_active_user)):
+async def add_friend_to_current_user(username_to_add: str = Form(...), current_user: UserInDB = Depends(get_current_user)):
     username_to_add = username_to_add.lower()
     user_to_add = db.get_firebase_user(username_to_add)
 
