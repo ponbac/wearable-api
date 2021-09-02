86ae67208c3536873dba26bb08128c4cf92bbf34
ponbac <ponbac@student.chalmers.se>
2021-09-02 02:56:34 +0200

    using the correct event

--- app/test.py
+++ app/test.py
@@ -14,16 +14,25 @@ async def on_message(message):
         return
 
     if message.content.startswith('$hello'):
-        total_members = 0
-        for guild in client.guilds:
-            for channel in guild.channels:
-                if type(channel) is discord.channel.VoiceChannel:
-                    members = channel.members
-                    print(f'{channel}: {len(members)}')
-                    for member in members:
-                        print(f'{member.nick} avatar={member.avatar_url}')
-                    total_members += len(channel.members)
         await message.channel.send(f'Hello to the {total_members} of you!')
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    print('---------------------------------------------------')
+    print(f'User: {member} changed!\n')
+
+    total_members = 0
+    for guild in client.guilds:
+        for channel in guild.channels:
+            if type(channel) is discord.channel.VoiceChannel:
+                members = channel.members
+                if len(members) > 0:
+                    print(f'{channel}: {len(members)}')
+                for member in members:
+                    print(f'{member.nick} avatar={member.avatar_url}')
+                total_members += len(channel.members)
+    print(f'\nTotal members online: {total_members}')
+    
 
 dotenv.load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
