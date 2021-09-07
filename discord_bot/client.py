27edfbb191b6bd610e49f38f1ef0a6a0a2c70683
Pontus Backman <ponbac@student.chalmers.se>
2021-09-07 19:16:49 +0200

    now provides more data

--- discord_bot/client.py
+++ discord_bot/client.py
@@ -2,28 +2,32 @@ import discord
 from deta import Deta
 from config import settings
 
+
 class DiscordClient(discord.Client):
+    def create_db_connection(self):
+        deta = Deta(settings.DETA_PROJECT_KEY)
+        return deta.Base('members')
+
     async def on_ready(self):
         print(f'Logged in as {self.user}')
 
     async def on_voice_state_update(self, member, before, after):
         #print('---------------------------------------------------')
-        #print(f'Before: {before}\nAfter: {after}')
-        
+        #print(f'Member:  {member}\nBefore: {before}\nAfter: {after}')
+
         if after.channel is None:
-            deta = Deta(settings.DETA_PROJECT_KEY)
-            db = deta.Base('members')
+            db = self.create_db_connection()
             print(f'{member.nick} left the voice channel!')
-            user = next(db.fetch({"nick": member.nick}))[0]
-            db.delete(user['key'])
-        elif before.channel == after.channel or before.channel is not None:
-            print(f'{member.nick} state changed')
+            db.delete(member.id)
         else:
-            deta = Deta(settings.DETA_PROJECT_KEY)
-            db = deta.Base('members')
-            print(f'{member.nick} joined {after.channel} avatar={str(member.avatar_url)}')
-            db.insert({'nick': member.nick, 'avatar': str(member.avatar_url_as(size=256))})
-        
+            db = self.create_db_connection()
+            print(
+                f'{member.nick} updated, in channel {after.channel} avatar={str(member.avatar_url)}')
+            db_object = {'nick': member.nick, 'avatar': str(
+                member.avatar_url_as(size=256)), 'channelId': str(after.channel.id), 'channelName': str(after.channel.name), 'isStreaming': bool(after.self_stream)}
+            #print(db_object)
+            db.put(db_object, str(member.id))
+
 # total_members = 0
 #         for guild in self.guilds:
 #             for channel in guild.channels:
